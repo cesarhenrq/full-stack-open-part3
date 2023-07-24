4cbe2e7d037d4296ca38dc4b812340853b4a72fd
cesarhenrq <cesaarhvb@gmail.com>
2023-07-24 03:23:26 -0300

    Exercise 3.6: Phonebook backend step6

--- index.js
+++ index.js
@@ -62,6 +62,20 @@ app.delete("/api/persons/:id", (req, res) => {
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+
+  const alreadyExists = persons.find((person) => person.name === body.name);
+
+  if (alreadyExists) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
   const person = {
     id: gereateId(),
     name: body.name,
