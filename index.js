2898653034277c4c7649610108a7457b065c542d
cesarhenrq <cesaarhvb@gmail.com>
2023-07-24 04:21:35 -0300

    Exercise 3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(morgan("tiny"));
+morgan.token("body", function (req) {
+  return JSON.stringify(req.body);
+});
 
 app.use(express.json());
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
