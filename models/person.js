118b27b57058ddb8a2e2e8f088846545c72d1aa2
cesarhenrq <cesaarhvb@gmail.com>
2023-07-26 06:46:41 -0300

    Exercise 3.14: Phonebook database, step2

--- models/person.js
+++ models/person.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI;
+console.log("connecting to", url);
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
 
 const personSchema = new mongoose.Schema({
   name: String,
